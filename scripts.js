739ac7e9b0149b9980831a3fbc54438171f392cb
manuelpayet <manuelpayet@gmail.com>
2011-11-29 16:31:10 +0000

    Extraction des paramètres de l'URL façon JQuery

--- scripts.js
+++ scripts.js
@@ -28,3 +28,25 @@ function extractUrlParams() {
   }
   return f;
 }
+
+$.extend({
+  getUrlVars: function() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for (var i = 0; i < hashes.length; i++) {
+      hash = hashes[i].split('=');
+      vars.push(hash[0]);
+      vars[hash[0]] = hash[1];
+    }
+    return vars;
+  },
+  getUrlVar: function(name) {
+    return $.getUrlVars()[name];
+  }
+});
+
+/**
+ * Exemples d'utilisation :
+ * p_qnoresult = $.getUrlVar('qnoresult');
+ * p_parameters = $.getUrlVars();
+ */

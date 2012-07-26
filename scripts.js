fd8172c36cea6ea1c2a3ec3fd760746a9d7937b8
manuelpayet <manuelpayet@gmail.com>
2012-07-26 15:14:09 +0000

    getUrlVars : mise à jour et suppression de code inutile

--- scripts.js
+++ scripts.js
@@ -36,18 +36,38 @@ function number() {
 
 jQuery(function($) {
   $.extend({
-    getUrlVars : function() {
-      var vars = [], hash;
-      var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-      for(var i = 0; i < hashes.length; i++) {
-        hash = hashes[i].split('=');
-        vars.push(hash[0]);
-        vars[hash[0]] = hash[1];
+    /**
+     * returns object with url variables
+     * hash parameters take precedence over search
+     * parameters
+     * @return {Object}
+     */
+    getUrlVars = function () {
+      var loc = document.location,
+        search = loc.search.replace('?', '').split('&'),
+        hash = loc.hash.replace('#', '').split('&'),
+        i, l, v, vars = {};
+  
+      for (i = 0, l = search.length; i < l; i++) {
+        v = search[i].split('=');
+        vars[v[0]] = decodeURI(v[1]);
       }
+  
+      for (i = 0, l = hash.length; i < l; i++) {
+        v = hash[i].split('=');
+        vars[v[0]] = decodeURI(v[1]);
+      }
+  
       return vars;
-    },
-    getUrlVar : function(name) {
-      return $.getUrlVars()[name];
+    }
+  
+    /**
+     * returns a single value from the getUrlVars hash
+     * @param {String} i The hash index
+     * @return {String}
+     */
+    getUrlVar = function (i) {
+      return $.getUrlVars()[i];
     }
   });
   $.fn.multicolorMe = function () {

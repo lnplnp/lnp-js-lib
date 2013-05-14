eadd9ab0985389e9ae689ba465f871694e6f674c
Manuel PAYET <manuelpayet@gmail.com>
2013-05-14 14:51:25 +0200

    Renommage de la variable "data" en "message"

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
-function logme(data) {
+function logme(message) {
   if (typeof console != 'undefined') {
     /* Commenter la ligne suivante pour faire taire tous les logme ;o) */
-    console.log(data);
+    console.log(message);
   }
 }
 
@@ -367,3 +367,4 @@ function RGBColor(color_string) {
 }
 =======
 }
+

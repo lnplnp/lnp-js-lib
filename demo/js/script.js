e47f724bb724735e22d436e8c4d4c053d12fe081
Manuel PAYET <manuelpayet@gmail.com>
2013-05-14 14:50:21 +0200

    Suppression des marques de merge foireux

--- demo/js/script.js
+++ demo/js/script.js
@@ -1,12 +1,9 @@
 $(document).ready(function() {
   $("p").multicolorMe();
-<<<<<<< HEAD
   
   assert(document.getElementById("results") != null, "The result list is here !");
-=======
   assert(document.getElementById('demo') != null, '"demo" element exists !');
   assert(document.getElementById('azer') != null, '"azer" element exists !');
   assert(document.getElementById('demo') != null, '"demo" element exists !');
   assert(document.getElementById('azer') != null, '"azer" element exists !');
->>>>>>> hotfix/FunctionAssert
 });
